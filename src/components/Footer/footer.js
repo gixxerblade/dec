b4a46b0d6742427e33d52bb78779a417e16fb19f
gixxerblade <sdclarkie@gmail.com>
2020-10-23 18:03:23 -0400

    updated footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,27 +1,55 @@
 import React from "react"
 import { FacebookCircle, Instagram } from "@styled-icons/boxicons-logos"
-import { BicycleGallery, BicycleShop } from "../../icons"
-const Footer = () => (
-  <>
-    <footer className="section">
-      <div className="columns">
-        <div className="column">
-          <h1 className="heading is-size-3 has-text-centered">Connect</h1>
-          <div className="social-container">
-            <FacebookCircle title="Facebook" className=" fb-circle" size={60} />
-            <Instagram title="Instagram" className=" instagram" size={50} />
+import styled from "styled-components"
+import {
+  BicycleGallery,
+  BicycleShop,
+  CapeFearCyclists,
+  CapeFearSorba,
+} from "../../icons"
+const Footer = () => {
+  return (
+    <>
+      <footer className="section">
+        <div className="columns">
+          <div className="column">
+            <h1 className="heading is-size-3 has-text-centered">Connect</h1>
+            <div className="social-container">
+              <FacebookCircle
+                title="Facebook"
+                className=" fb-circle"
+                size={60}
+              />
+              <Instagram title="Instagram" className=" instagram" size={50} />
+            </div>
+          </div>
+          <div className="column">
+            <h1 className="heading is-size-3 has-text-centered">Links</h1>
+            <div className="columns has-text-centered is-center">
+              <div className="column">
+                <a href="https://www.bicycle-gallery.com/">
+                  <BicycleGallery />
+                </a>
+              </div>
+              <div className="column">
+                <BicycleShop />
+              </div>
+              <div className="column">
+                <CapeFearCyclists />
+              </div>
+              <div className="column">
+                <CapeFearSorba />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="column">
-          <h1 className="heading is-size-3 has-text-centered">Links</h1>
+        <div className="copyright">
+          <h2 className="heading">
+            &copy;{new Date().getFullYear()} Down East Cyclists
+          </h2>
         </div>
-      </div>
-      <div className="copyright">
-        <h2 className="heading">
-          &copy;{new Date().getFullYear()} Down East Cyclists
-        </h2>
-      </div>
-    </footer>
-  </>
-)
+      </footer>
+    </>
+  )
+}
 export default Footer
