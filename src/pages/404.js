bd8d1be65ecec895898ec26238b3cc51dc597865
gixxerblade <sdclarkie@gmail.com>
2020-11-02 16:44:51 -0500

    updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,34 @@
 import React from "react"
-
+import Dec from "../icons"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
+    <br />
+    <br />
+    <div className="section">
+      <div className="container">
+        <div className="container">
+          <h1 className="title is-1 has-text-black has-text-centered">
+            Page Not Found
+          </h1>
+          <p className="title is-3 has-text-black has-text-centered">
+            You just hit a route that doesn&#39;t exist...
+            <span role="img" aria-label="sadface">
+              😥
+            </span>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="section">
+      <div className="container has-text-centered">
+        <Dec shadow="#000" cyclists="#000" details="gray" height="300" />
+      </div>
+    </div>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
