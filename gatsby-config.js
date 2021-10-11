416f0cf8763ea4969b661c120557c10a6550fe87
gixxerblade <sdclarkie@gmail.com>
2021-10-11 09:24:48 -0400

    Update authentication part of logging in to change status.

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,5 +80,13 @@ module.exports = {
         downloadLocal: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { IdentityContextProvider } from "react-netlify-identity-widget";
 import Nav from "./Nav"
 import Footer from "./Footer"
 import "../assets/layout.scss"
@@ -16,17 +17,17 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <IdentityContextProvider url="https://www.downeastcyclists.com">
       <div className="sticky-wrapper sticky">
         <Nav
-          siteTitle={data.site.siteMetadata?.title || `Down East Cyclists`}
+          siteTitle={data.site.siteMetadata?.title ?? `Down East Cyclists`}
         />
       </div>
       <div className="container">
         <main>{children}</main>
         <Footer />
       </div>
-    </>
+    </IdentityContextProvider>
   )
 }
 
